8d4737e95e6d685938f1efb5d2319eda1a2e5c1e
bigbird231 <396016240@qq.com>
2017-04-20 12:48:51 +0800

    modify face

--- src/js/index.js
+++ src/js/index.js
@@ -6,12 +6,16 @@
     var bindTrigger,bindSearch,beginPage;
 
     bindTrigger=function(){
-        var buttons,showType,i,l;
+        var buttons,showType,i,l,lastTrigger;
         stockCode="600570.SS";
         showType=["分时","五日","1分钟","5分钟","15分钟","30分钟","60分钟","日K","周K","月K","年K"];
-        buttons=document.querySelectorAll(".button-wrap button");
+        buttons=document.querySelectorAll("#sg-triggerwrap a");
+        lastTrigger=buttons[0];
         for(i=0,l=showType.length;i<l;i++)(function(index){
             buttons[index].addEventListener("click",function(){
+                lastTrigger.className="";
+                lastTrigger=buttons[index];
+                lastTrigger.className="active";
                 StockGraph.draw(showType[index],stockCode);
             });
         })(i);
@@ -19,11 +23,13 @@
 
     bindSearch=function(){
         var input,search;
-        input=document.querySelector("input");
-        search=document.querySelector(".search");
+        input=document.querySelector(".sg-input");
+        search=document.querySelector(".sg-submit");
         search.addEventListener("click",function(){
-            stockCode=input.value;
-            StockGraph.draw("日K",stockCode);
+            if(input.value!=""){
+                stockCode=input.value;
+                StockGraph.draw("日K",stockCode);
+            }
         });
     };
 
